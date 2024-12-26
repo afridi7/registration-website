f122b650b3e71a19d457ec035d363610cf7bb9cc
afridi7 <afridikhondkar7@gmail.com>
2024-12-26 19:15:19 +0530

    updates, footer, logo, submit, events

--- script.js
+++ script.js
@@ -133,3 +133,71 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+function enable_loader(){
+    document.getElementsByClassName("wrapper")[0].style.filter = "blur(3px)";
+    document.getElementsByClassName("loader")[0].style.display = "block";
+}
+function disable_loader(){
+    document.getElementsByClassName("wrapper")[0].style.filter = "blur(0px)";
+    document.getElementsByClassName("loader")[0].style.display = "none";
+}
+function submitForm(){
+// Define the parameters
+var evob = document.getElementById("event-name")
+const event_name = evob.innerHTML;
+var cob = document.getElementsByName("College")[0]
+const college = cob.value;
+var eob=  document.getElementsByName("Email")[0]
+const email =eob.value;
+var mob=  document.getElementsByName("Phone")[0]
+const mobile = mob.value;
+var cousceob= document.getElementsByClassName("selected")[0]
+const course = cousceob.innerText;
+var participant1ob = document.getElementById("Participant1");
+var participant2ob = document.getElementById("Participant2");
+var participant3ob = document.getElementById("Participant3");
+var participant4ob = document.getElementById("Participant4");
+const participant1 = participant1ob.value;
+const participant2 = participant2ob?participant2ob.value:"";
+const participant3 = participant3ob?participant3ob.value:"";
+const participant4 = participant4ob?participant4ob.value:"";
+// Construct the URL with parameters
+const url = `https://script.google.com/macros/s/AKfycbwakhuqL62V7dsL3QCnFkqBcyFUGANn_IEE2x_mw3mHVPEafxKw21-erwJekRmIK7x1/exec?event=${encodeURIComponent(event_name)}&college=${encodeURIComponent(college)}&email=${encodeURIComponent(email)}&mobile=${encodeURIComponent(mobile)}&participant1=${encodeURIComponent(participant1)}&participant2=${encodeURIComponent(participant2)}&participant3=${encodeURIComponent(participant3)}&participant4=${encodeURIComponent(participant4)}&course=${encodeURIComponent(course)}`;
+// console.log(url);
+// Make the GET request
+enable_loader()
+fetch(url,{
+    redirect: "follow",
+    method: "GET",
+  })
+  .then(response => response.json())  // Assuming the response is JSON
+  .then(data => {
+    disable_loader()
+    // console.log("Success:", data);
+    if(data.status=="failed"){
+        alert("Missing Fields")
+    }
+    else{
+        alert("Form Submitted Successfully")
+        evob.value = "";
+        cob.value ="";
+        eob.value ="";
+      participant1ob.value="";
+       participant2ob.value="";
+       participant3ob.value="";
+      participant4ob.value="";
+      mob.value="";
+    }
+    
+      // Handle the response data
+  })
+  .catch(error => {
+    console.error("Error:", error);  // Handle any errors
+    disable_loader()
+    alert("Failed! Try Again")
+  });
+
+
+}
