2a34af0baa1c2f55b8bb61fe01a1cee5d57acee3
afridi7 <afridikhondkar7@gmail.com>
2025-01-14 17:50:55 +0530

    updates

--- script.js
+++ script.js
@@ -70,6 +70,8 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location.href = 'register-pages/register-citd.html';
         } else if (selectedEvent.includes('Quick Think')) {
             window.location.href = 'register-pages/register-qt.html';
+        } else if (selectedEvent.includes('Mr. & Ms. Technovation')) {
+            window.location.href = 'register-pages/register-m&mt.html';
         } else {
             alert('Please select a valid event.');
         }
@@ -101,6 +103,9 @@ document.getElementById('qtImage').addEventListener('click', function () {
 document.getElementById('qqImage').addEventListener('click', function () {
     location.href = 'events/qq.html';
 });
+document.getElementById('m&mtImage').addEventListener('click', function () {
+    location.href = 'events/m&mt.html';
+});
 
 
 
@@ -109,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const selectedEvent = document.querySelector('.selected');
 
     nextButton.addEventListener('click', function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
         // Prevent form submission by default
 
         if (selectedEvent.textContent === 'Select an Event') {
@@ -125,75 +130,77 @@ document.addEventListener('DOMContentLoaded', function () {
                 window.location.href = 'register-pages/register-citd.html';
             } else if (selectedEvent.textContent.includes('Quick Think')) {
                 window.location.href = 'register-pages/register-qt.html';
+            } else if (selectedEvent.textContent.includes('Mr. & Ms. Technovation')) {
+                window.location.href = 'register-pages/register-m&mt.html';
             }
         }
     });
 });
 
 
-function enable_loader(){
+function enable_loader() {
     document.getElementsByClassName("wrapper")[0].style.filter = "blur(3px)";
     document.getElementsByClassName("loader")[0].style.display = "block";
 }
-function disable_loader(){
+function disable_loader() {
     document.getElementsByClassName("wrapper")[0].style.filter = "blur(0px)";
     document.getElementsByClassName("loader")[0].style.display = "none";
 }
-function submitForm(){
-// Define the parameters
-var evob = document.getElementById("event-name")
-const event_name = evob.innerHTML;
-var cob = document.getElementsByName("College")[0]
-const college = cob.value;
-var eob=  document.getElementsByName("Email")[0]
-const email =eob.value;
-var mob=  document.getElementsByName("Phone")[0]
-const mobile = mob.value;
-var cousceob= document.getElementsByClassName("selected")[0]
-const course = cousceob.innerText;
-var participant1ob = document.getElementById("Participant1");
-var participant2ob = document.getElementById("Participant2");
-var participant3ob = document.getElementById("Participant3");
-var participant4ob = document.getElementById("Participant4");
-const participant1 = participant1ob.value;
-const participant2 = participant2ob?participant2ob.value:"";
-const participant3 = participant3ob?participant3ob.value:"";
-const participant4 = participant4ob?participant4ob.value:"";
-// Construct the URL with parameters
-const url = `https://script.google.com/macros/s/AKfycbwakhuqL62V7dsL3QCnFkqBcyFUGANn_IEE2x_mw3mHVPEafxKw21-erwJekRmIK7x1/exec?event=${encodeURIComponent(event_name)}&college=${encodeURIComponent(college)}&email=${encodeURIComponent(email)}&mobile=${encodeURIComponent(mobile)}&participant1=${encodeURIComponent(participant1)}&participant2=${encodeURIComponent(participant2)}&participant3=${encodeURIComponent(participant3)}&participant4=${encodeURIComponent(participant4)}&course=${encodeURIComponent(course)}`;
-// console.log(url);
-// Make the GET request
-enable_loader()
-fetch(url,{
-    redirect: "follow",
-    method: "GET",
-  })
-  .then(response => response.json())  // Assuming the response is JSON
-  .then(data => {
-    disable_loader()
-    // console.log("Success:", data);
-    if(data.status=="failed"){
-        alert("Missing Fields")
-    }
-    else{
-        alert("Form Submitted Successfully")
-        evob.value = "";
-        cob.value ="";
-        eob.value ="";
-      participant1ob.value="";
-       participant2ob.value="";
-       participant3ob.value="";
-      participant4ob.value="";
-      mob.value="";
-    }
-    
-      // Handle the response data
-  })
-  .catch(error => {
-    console.error("Error:", error);  // Handle any errors
-    disable_loader()
-    alert("Failed! Try Again")
-  });
+function submitForm() {
+    // Define the parameters
+    var evob = document.getElementById("event-name")
+    const event_name = evob.innerHTML;
+    var cob = document.getElementsByName("College")[0]
+    const college = cob.value;
+    var eob = document.getElementsByName("Email")[0]
+    const email = eob.value;
+    var mob = document.getElementsByName("Phone")[0]
+    const mobile = mob.value;
+    var cousceob = document.getElementsByClassName("selected")[0]
+    const course = cousceob.innerText;
+    var participant1ob = document.getElementById("Participant1");
+    var participant2ob = document.getElementById("Participant2");
+    var participant3ob = document.getElementById("Participant3");
+    var participant4ob = document.getElementById("Participant4");
+    const participant1 = participant1ob.value;
+    const participant2 = participant2ob ? participant2ob.value : "";
+    const participant3 = participant3ob ? participant3ob.value : "";
+    const participant4 = participant4ob ? participant4ob.value : "";
+    // Construct the URL with parameters
+    const url = `https://script.google.com/macros/s/AKfycbwakhuqL62V7dsL3QCnFkqBcyFUGANn_IEE2x_mw3mHVPEafxKw21-erwJekRmIK7x1/exec?event=${encodeURIComponent(event_name)}&college=${encodeURIComponent(college)}&email=${encodeURIComponent(email)}&mobile=${encodeURIComponent(mobile)}&participant1=${encodeURIComponent(participant1)}&participant2=${encodeURIComponent(participant2)}&participant3=${encodeURIComponent(participant3)}&participant4=${encodeURIComponent(participant4)}&course=${encodeURIComponent(course)}`;
+    // console.log(url);
+    // Make the GET request
+    enable_loader()
+    fetch(url, {
+        redirect: "follow",
+        method: "GET",
+    })
+        .then(response => response.json())  // Assuming the response is JSON
+        .then(data => {
+            disable_loader()
+            // console.log("Success:", data);
+            if (data.status == "failed") {
+                alert("Missing Fields")
+            }
+            else {
+                alert("Form Submitted Successfully")
+                evob.value = "";
+                cob.value = "";
+                eob.value = "";
+                participant1ob.value = "";
+                participant2ob.value = "";
+                participant3ob.value = "";
+                participant4ob.value = "";
+                mob.value = "";
+            }
+
+            // Handle the response data
+        })
+        .catch(error => {
+            console.error("Error:", error);  // Handle any errors
+            disable_loader()
+            
+        });
 
 
 }
